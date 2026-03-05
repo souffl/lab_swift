e97f646fd5464e8f39bdb893b0f28baa4f2fcc4a
Ekaterina Berendiugina <kta.berendugina@mail.ru>
2026-03-05 12:04:04 +0300

    Initial Commit

---
+++ lab_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab_swift
+//
+//  Created by Екатерина Берендюгина on 05.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
